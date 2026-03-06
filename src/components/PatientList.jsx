141f90bff6f2a95ab434dcd7fb540f829dee21ef
Juan Genova <juang@live.com.ar>
2026-03-06 10:06:57 +0000

    feat: agregar funcionalidad para crear pacientes nuevos

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,18 +1,48 @@
+import { useState } from "react";
 
 function PatientList() {
-    const Patients = [
-        { id: 1, name: 'Juan Maria', phone: '1234' },
-        { id: 2, name: 'Rodrigo', phone: '54321' },
-        { id: 3, name: 'Diego', phone: '999' }
-    ]
+    const [patients, setPatients] = useState([])
+    const [name, setName] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [phone, setPhone] = useState('')
+
+    function handleAddPatient(e) {
+        e.preventDefault()
+        const id = Date.now()
+        const newPatient = {
+            id: id, name: name, lastname: lastname, phone: phone
+        }
+        setPatients([...patients, newPatient])
+        setName('')
+        setLastname('')
+        setPhone('')
+    }
+
 
     return (
         <div>
-            <h2>Mis pacientes</h2>
+            <h2>Mis pacientes!!</h2>
+            <h3>Agregar Pacientes</h3>
+            <form onSubmit={handleAddPatient}>
+                <label>
+                    Nombre: <input value={name}
+                        onChange={e => setName(e.target.value)} />
+                </label>
+                <label>
+                    Apellido: <input value={lastname}
+                        onChange={e => setLastname(e.target.value)} />
+                </label>
+                <label>
+                    Tel: <input value={phone}
+                        onChange={e => setPhone(e.target.value)} />
+                </label>
+                <button type="submit">Agregar</button>
+            </form>
+
             <ul>
-                {Patients.map(patient => (
+                {patients.map(patient => (
                     <li key={patient.id}>
-                        {patient.name} - {patient.phone}
+                        Nombre y apellido:  {patient.name} {patient.lastname} - Telefono: {patient.phone}
                     </li>
                 ))}
             </ul>
